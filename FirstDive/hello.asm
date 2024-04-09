42ff44d36cc9a2a7db58cfe51e516960e102dfd6
Tudor Popov <supreme.notfound@gmail.com>
2024-04-09 15:13:16 +0300

    made some changes for my lab works

--- FirstDive/hello.asm
+++ FirstDive/hello.asm
@@ -1,24 +1,12 @@
-default rel         ; Use RIP-relative addressing like [rel msg] by default
-global WinMain
-extern ExitProcess  ; external functions in system libraries 
-extern MessageBoxA
+    global _main
+    extern _printf
 
-section .data 
-title:  db 'I fucking did It!!', 0
-msg:    db 'Hello world!', 0
-
-section .text
-WinMain:
-    sub rsp, 28h      ; reserve shadow space and make RSP%16 == 0
-    mov rcx, 0       ; hWnd = HWND_DESKTOP
-    lea rdx,[msg]    ; LPCSTR lpText
-    lea r8,[title]   ; LPCSTR lpCaption
-    mov r9d, 0       ; uType = MB_OK
-    call MessageBoxA
-
-    mov  ecx,eax        ; exit status = return value of MessageBoxA
-    call ExitProcess
-
-    add rsp, 28h       ; if you were going to ret, restore RSP
-
-    hlt     ; privileged instruction that crashes if ever reached.
+    section .data
+    message: db 'Hello World', 0xA, 0
+    
+    section .text
+_main:
+        push    message
+        call    _printf
+        add     esp, 4
+        ret 
--- FirstDive/inout.asm
+++ FirstDive/inout.asm
@@ -1,22 +1,49 @@
-bits 64
-default rel
+section .data
+        prompt1 db "Enter the first integer: ", 0
+        prompt2 db "Enter the second integer: ", 0
+        format db "%d", 0
+        result db "The sum is: %d", 0
 
-segment .data
-    msg db "Hello world!", 0xd, 0xa, 0
+    section .bss
+        num1 resd 1
+        num2 resd 1
+        sum resd 1
 
-segment .text
-global main
-extern ExitProcess
+    section .text
+        extern  _printf, _scanf, _exit
 
-extern printf
+    global _main
 
-main:
-    push    rbp
-    mov     rbp, rsp
-    sub     rsp, 32
+    _main:
 
-    lea     rcx, [msg]
-    call    printf
+        push prompt1
+        call _printf
+        add esp, 4
 
-    xor     rax, rax
-    call    ExitProcess
+        lea eax, [num1]
+        push eax
+        push format
+        call _scanf
+        add esp, 8
+
+        push prompt2
+        call _printf
+        add esp, 4
+
+        lea eax, [num2]
+        push eax
+        push format
+        call _scanf
+        add esp, 8
+
+        mov eax, [num1]
+        add eax, [num2]
+        mov [sum], eax
+
+        push dword [sum]
+        push result
+        call _printf
+        add esp, 8
+
+        push 0
+        call _exit
